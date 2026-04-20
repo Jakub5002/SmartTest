75186e5f98fc595660eef923e509c64de9f70671
Wiktor <wiktor.data5@gmail.com>
2026-04-20 11:55:17 +0200

    Poczatkowa struktura z folderami backendu i frontendu

---
+++ backend/src/main/java/com/example/backend/SmartTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTestApplication.class, args);
+    }
+
+}
