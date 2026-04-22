9bcdd6467786cd5c935b88404e4ee175a5f12d2a
Wiktor <wiktor.data5@gmail.com>
2026-04-22 15:39:14 +0200

    Drobne zmiany + dodanie encji exam

--- backend/src/main/java/com/example/backend/model/Exam.java
+++ backend/src/main/java/com/example/backend/model/Exam.java
@@ -1,4 +1,27 @@
 package com.example.backend.model;
+import jakarta.persistence.*; //JPA - Java Persistence API | do operowania na bazie danych
+import java.util.UUID; // biblioteka do generowania idnetyfikatorów
+import java.util.Objects;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "exams")
+@Getter @Setter
 public class Exam {
+    @Id //pole jako klucz główny (Primary Key)
+    @GeneratedValue(strategy = GenerationType.UUID) //Instruuje bazę danych, aby automatycznie generował identyfikator
+    private UUID _id;
+
+    @Column(nullable = false, unique = true)
+    private String _title;
+
+    @Column(nullable = false)
+    private String _duration_minutes;
+
+    private UUID _created_by;
+
+    @Column(name = "created_at")
+    private boolean _is_active;
+
 }
--- backend/src/main/java/com/example/backend/model/Question.java
+++ backend/src/main/java/com/example/backend/model/Question.java
@@ -1,4 +1,19 @@
 package com.example.backend.model;
+import jakarta.persistence.*; //JPA - Java Persistence API | do operowania na bazie danych
+import java.util.UUID; // biblioteka do generowania idnetyfikatorów
+import java.util.Objects;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "questions")
+@Getter @Setter
 public class Question {
+    @Id //pole jako klucz główny (Primary Key)
+    @GeneratedValue(strategy = GenerationType.UUID) //Instruuje bazę danych, aby automatycznie generował identyfikator
+    private UUID _id;
+
+
+    private UUID _exam_id;
+
 }
