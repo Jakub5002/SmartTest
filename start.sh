13f6ddfe2f73d529fe2faad273aa846d0c3bdc0a
Jakub <jakub.wol@onet.pl>
2026-06-15 21:50:43 +0200

    Poprawa uruchumienia na Linux

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+ROOT_DIR=$(pwd)
+
+echo "Restartowanie portu..."
+fuser -k 8080/tcp 2>/dev/null
+
 echo "Uruchamianie backendu..."
-cd backend
+cd "$ROOT_DIR/backend"
 mvn spring-boot:run &
 
 echo "Uruchamianie frontendu..."
-cd ../frontend
-npm install
-npm start
+sleep 10
+
+cd "$ROOT_DIR/frontend"
+npm run dev
